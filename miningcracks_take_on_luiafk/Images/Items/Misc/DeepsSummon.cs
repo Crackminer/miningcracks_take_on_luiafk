2d67f95b8409e3201e912ece3a357edbbf20bdba
Crackminer <87083544+Crackminer@users.noreply.github.com>
2022-07-21 19:42:46 +0200

    me dumb

--- miningcracks_take_on_luiafk/Images/Items/Misc/DeepsSummon.cs
+++ miningcracks_take_on_luiafk/Images/Items/Misc/DeepsSummon.cs
@@ -94,10 +94,6 @@ namespace miningcracks_take_on_luiafk.Images.Items.Misc
             Recipe recipeBack = Recipe.Create(3202, 1);
             recipeBack.AddIngredient<DeepsSummon>(1);
             recipeBack.Register();
-            
-            Recipe recipeForth = base.CreateRecipe(1);
-            recipeForth.AddIngredient(3202, 1);
-            recipeForth.Register();
         }
     }
 }
